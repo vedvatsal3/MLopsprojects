97ee67db6393f4238f735e3039791d98ae40d92c
Your Name <vedvatsal3@gmail.com>
2024-07-24 12:09:57 +0530

    Remove nested repository and add files

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "MLopsprojects"
+AUTHOR_USER_NAME = "vedvatsal3"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "vatsalved52862@gmail.com"
 
 
 setuptools.setup(
